800a992043b3980643cfb96be63a9f39a3db73d9
Sarovar <iamsarovarjs@gmail.com>
2019-08-08 21:45:00 +0530

    second-commit

---
+++ config/db.js
@@ -0,0 +1,19 @@
+// to set up the connection to mongo db
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true
+    });
+    console.log('MogoDB Connected');
+  } catch (err) {
+    console.log(err.message);
+    // exit procees with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//connect the database
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API RUNNING');
 });
+
+// define route
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 // set the port number
 const PORT = process.env.PORT || 5000;
