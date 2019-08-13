65c942a0cf9664118445ac9cf906118d3b50a117
Sarovar <iamsarovarjs@gmail.com>
2019-08-13 21:06:20 +0530

    eight commit

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,100 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
+
+const Register = ({ setAlert }) => {
+  // making the state using hooks
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+  const { name, email, password, password2 } = formData;
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+
+    if (password !== password2) {
+      setAlert('Passwords do not ,match', 'danger');
+    } else {
+      console.log(formData);
+      console.log('Success');
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Create Your Account
+      </p>
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            required
+            onChange={e => onChange(e)}
+            value={name}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            required
+            onChange={e => onChange(e)}
+            value={email}
+          />
+          <small className='form-text'>
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='6'
+            required
+            onChange={e => onChange(e)}
+            value={password}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='password2'
+            minLength='6'
+            required
+            onChange={e => onChange(e)}
+            value={password2}
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Register' />
+      </form>
+      <p className='my-1'>
+        Already have an account? <Link to='/login'>Sign In</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default connect(
+  null,
+  { setAlert }
+)(Register);
+
+// prop types,  setAlter is of type function
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
---
+++ client/src/reducers/index.js
@@ -0,0 +1,7 @@
+// this is the root reducer
+import { combineReducers } from 'redux';
+import alert from './alert';
+
+export default combineReducers({
+  alert
+});
