a65a2653edd8b7f91454711accc294cc96ef84bb
Sarovar <iamsarovarjs@gmail.com>
2019-08-20 13:58:35 +0530

    14 commit problem with delete post, addLike and removeLike not updating correctly

---
+++ client/src/components/posts/PostItem.js
@@ -0,0 +1,73 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addLike, removeLike, deletePost } from '../../actions/post';
+
+const PostItem = ({
+  addLike,
+  removeLike,
+  deletePost,
+  auth,
+  post: { _id, text, name, avatar, user, like, comments, date }
+}) => {
+  return (
+    <div class='post bg-white p-1 my-1'>
+      <div>
+        <a href='profile.html'>
+          <img class='round-img' src={avatar} alt='' />
+          <h4>{name}</h4>
+        </a>
+      </div>
+      <div>
+        <p class='my-1'>{text}</p>
+        <p class='post-date'>
+          Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
+        </p>
+        <button type='button' class='btn btn-light' onClick={e => addLike(_id)}>
+          <i class='fas fa-thumbs-up' />
+          <span> {like.length}</span>
+        </button>
+        <button
+          type='button'
+          class='btn btn-light'
+          onClick={e => removeLike(_id)}
+        >
+          <i class='fas fa-thumbs-down' />
+        </button>
+
+        <Link to={`/post/${_id}`} class='btn btn-primary'>
+          Discussion <span class='comment-count'> {comments.length}</span>
+        </Link>
+
+        {!auth.loading && user === auth.user._id && (
+          <button
+            type='button'
+            class='btn btn-danger'
+            // onClick={e => deletePost(_id)}
+          >
+            <i class='fas fa-times' />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+PostItem.propTypes = {
+  post: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
+  deletePost: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { addLike, removeLike, deletePost }
+)(PostItem);
